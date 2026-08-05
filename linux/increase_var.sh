39b97efc18878f137be42ee08049855310ba8cbb
maalim-mohamed-zakaria <maalim.mohamedzakaria@etu.uae.ac.ma>
2026-08-05 11:59:36 +0100

    Update increase_var.sh

--- linux/increase_var.sh
+++ linux/increase_var.sh
@@ -1,1 +1,8 @@
-b
+#!/bin/bash
+
+if lvextend -L +2M /dev/rl/var -y; then
+    xfs_growfs /var
+    logger -t increase_var "Zabbix automatically extended /var by 2MB"
+else
+    logger -t increase_var "Zabbix failed to extend /var //espace insuffisant// "
+fi
