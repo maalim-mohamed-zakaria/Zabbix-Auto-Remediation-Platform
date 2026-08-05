5ea9981940148ac84c08a4791639db409b43c27d
maalim-mohamed-zakaria <maalim.mohamedzakaria@etu.uae.ac.ma>
2026-08-05 11:42:04 +0100

    Update apache_restart.sh

--- linux/apache_restart.sh
+++ linux/apache_restart.sh
@@ -1,1 +1,13 @@
-d
+#!/bin/bash
+
+echo "$(date) SCRIPT EXECUTED" >> /tmp/restart_test.log
+
+sudo systemctl restart httpd
+
+if sudo systemctl is-active --quiet httpd; then
+    echo "Zabbix: Apache service restarted succesfully." >> /tmp/restart_test.log
+    exit 0
+else
+    echo "Zabbix: Failed to restart Apache service." >> /tmp/restart_test.log
+    exit 1
+fi
